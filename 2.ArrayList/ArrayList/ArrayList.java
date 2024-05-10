5af2bb61e30d361a6a93c06e791095adea088f9a
Rachi3 <rlagusdn715@gmail.com>
2024-05-11 02:34:26 +0900

    생성후 현재 저장된 위치 나타내는 변수생성

---
+++ 2.ArrayList/ArrayList/ArrayList.java
@@ -0,0 +1,7 @@
+package ArrayList;
+
+public class ArrayList {
+    private Object []obj=new Object[100];//처음 실행시 Object 배열 생성
+    private int nowIdx=0;//현재 어디까지 저장되어있는지 나타내는 변수생성
+    
+}
---
+++ 2.ArrayList/ArrayList/Main.java
@@ -0,0 +1,11 @@
+package ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList list=new ArrayList();
+
+        list.add(1);
+
+        System.out.println(list);
+    }
+}
